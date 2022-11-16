af577691e50db2b11bb31a1bd42a30b1d5c941df
Wilhelm <felipe.wilham@hotmail.com>
2022-11-16 17:21:58 -0300

    adicionando charset na conexao com o banco

--- conexao.php
+++ conexao.php
@@ -5,6 +5,9 @@ $usuario = "root";
 $senha = "";
 
 $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+$mysqli->set_charset("utf8");
+
 if ($mysqli->connect_errno) {
     echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
